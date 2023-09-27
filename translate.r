d1824acf7aa41817be5ffa6e3dce8a4e1e9e9681
pkumath <jnwengang@163.com>
2023-09-27 15:09:32 -0400

    translate

--- translate.r
+++ translate.r
@@ -1,238 +1,133 @@
-library(tidyverse)
-library(stringr)
+library(dplyr)
+library(tidyr)
 
 # Read data
 data_2017_2021 <- read.csv("data/data_2017_2021.csv")
 data_2022_2023 <- read.csv("data/data_2022_2023.csv")
 
 # For the "Apparatus" column, replace "VT_1", "VT_2" by "VT1", "VT2"
-data_2022_2023$Apparatus <- str_replace_all(data_2022_2023$Apparatus, c("VT_1" = "VT1", "VT_2" = "VT2"))
+data_2022_2023$Apparatus <- ifelse(data_2022_2023$Apparatus == "VT_1", "VT1",
+                                   ifelse(data_2022_2023$Apparatus == "VT_2", "VT2",
+                                          ifelse(data_2022_2023$Apparatus == "hb", "HB", data_2022_2023$Apparatus)))
+
 # Filter out NaN in the "Score" column
-data_2022_2023 <- filter(data_2022_2023, !is.na(Score))
+data_2022_2023 <- subset(data_2022_2023, !is.na(Score))
 
 # Split data_2022_2023 by Gender
-men_athletes_data <- filter(data_2022_2023, Gender == "m")
-women_athletes_data <- filter(data_2022_2023, Gender == "w")
+men_athletes_data <- subset(data_2022_2023, Gender == "m")
+women_athletes_data <- subset(data_2022_2023, Gender == "w")
 
 # Split men_athletes_data and women_athletes_data by "Country" into 2 lists
 men_country_athlete_list <- split(men_athletes_data, men_athletes_data$Country)
 women_country_athlete_list <- split(women_athletes_data, women_athletes_data$Country)
 
-# For men_country_athlete_dict and women_country_athlete_dict, select the top 9 countries with most entries
+# Select the top 12 countries with the most entries for both men and women
 k <- 12
-men_top_12_countries <- head(sort(table(men_athletes_data$Country), decreasing=TRUE), k)
-women_top_12_countries <- head(sort(table(women_athletes_data$Country), decreasing=TRUE), k)
-men_country_athlete_list_12 <- men_country_athlete_list[names(men_country_athlete_list) %in% names(men_top_12_countries)]
-women_country_athlete_list_12 <- women_country_athlete_list[names(women_country_athlete_list) %in% names(women_top_12_countries)]
+men_top_12_countries <- names(sort(table(men_athletes_data$Country), decreasing = TRUE))[1:k]
+women_top_12_countries <- names(sort(table(women_athletes_data$Country), decreasing = TRUE))[1:k]
 
-# Determine a 5-person team for each country based on the top 5 athletes who appear most frequently
-for (country in names(men_country_athlete_list_12)) {
-  top_5_athletes <- head(sort(table(men_country_athlete_list_12[[country]]$FirstName), decreasing=TRUE), 5)
-  men_country_athlete_list_12[[country]] <- filter(men_country_athlete_list_12[[country]], FirstName %in% names(top_5_athletes))
-}
-for (country in names(women_country_athlete_list_12)) {
-  top_5_athletes <- head(sort(table(women_country_athlete_list_12[[country]]$FirstName), decreasing=TRUE), 5)
-  women_country_athlete_list_12[[country]] <- filter(women_country_athlete_list_12[[country]], FirstName %in% names(top_5_athletes))
-}
+# Filter the men_country_athlete_list and women_country_athlete_list by the top 12 countries
+men_country_athlete_list_12 <- men_country_athlete_list[men_top_12_countries]
+women_country_athlete_list_12 <- women_country_athlete_list[women_top_12_countries]
 
-# Group data by "Apparatus" and calculate average score, then replace original data
-for (country in names(men_country_athlete_list_12)) {
-  men_country_athlete_list_12[[country]] <- men_country_athlete_list_12[[country]] %>%
-    group_by(FirstName, Apparatus) %>%
-    summarize(Score = mean(Score)) %>%
-    ungroup()
-}
-for (country in names(women_country_athlete_list_12)) {
-  women_country_athlete_list_12[[country]] <- women_country_athlete_list_12[[country]] %>%
-    group_by(FirstName, Apparatus) %>%
-    summarize(Score = mean(Score)) %>%
-    ungroup()
-}
-# Identify all apparatuses
-apparatuses <- unique(data_2017_2021$Apparatus)
-
-# Men's qualifying stage
-apparatus_list <- apparatuses
-men_qualifying_scores <- list()
-selected_athletes <- c()
-
-for (country in names(men_country_athlete_list_12)) {
-  country_scores <- c()
-  country_athlete_data <- men_country_athlete_list_12[[country]]
-  selected_athlete <- country_athlete_data %>%
-    group_by(FirstName) %>%
-    summarize(total_score = sum(Score)) %>%
-    arrange(-total_score) %>%
-    slice(1) %>%
-    pull(FirstName)
+# Define a function to process the data for each gender
+process_data <- function(data_list, apparatus_list) {
+  for (country in names(data_list)) {
+    data <- data_list[[country]]
+    top_5_athletes <- names(sort(table(data$FirstName), decreasing = TRUE))[1:5]
+    data <- subset(data, FirstName %in% top_5_athletes)
+    
+    data <- data %>%
+      group_by(FirstName, Apparatus) %>%
+      summarize(Score = mean(Score)) %>%
+      ungroup()
+    
+    data_list[[country]] <- data
+  }
   
-  selected_athletes <- c(selected_athletes, selected_athlete)
-  
-  for (apparatus in apparatus_list) {
-    apparatus_data <- filter(country_athlete_data, Apparatus == apparatus)
-    if (nrow(apparatus_data) == 0) {
-      country_scores <- c(country_scores, 10)
-    } else {
-      top_2_athletes <- apparatus_data %>%
+  # Qualifying scores
+  qualifying_scores <- lapply(names(data_list), function(country) {
+    country_data <- data_list[[country]]
+    sapply(apparatus_list, function(apparatus) {
+      apparatus_data <- subset(country_data, Apparatus == apparatus)
+      if (nrow(apparatus_data) == 0) return(10)
+      
+      top_2_athletes_scores <- tail(sort(apparatus_data$Score), 2)
+      if (length(top_2_athletes_scores) < 2) return(mean(top_2_athletes_scores))
+      
+      selected_athlete <- country_data %>%
         group_by(FirstName) %>%
-        summarize(average_score = mean(Score)) %>%
-        arrange(-average_score) %>%
-        slice(1:2) %>%
-        pull(average_score)
+        summarize(total_score = sum(Score)) %>%
+        arrange(-total_score) %>%
+        slice(1) %>%
+        pull(FirstName)
       
-      selected_score <- filter(country_athlete_data, FirstName == selected_athlete, Apparatus == apparatus) %>%
-        pull(Score)
+      selected_score <- subset(country_data, FirstName == selected_athlete & Apparatus == apparatus)$Score
       
-      top_3_scores <- c(top_2_athletes, selected_score)
-      country_scores <- c(country_scores, mean(top_3_scores, na.rm = TRUE))
-    }
-  }
+      mean(c(top_2_athletes_scores, selected_score))
+    })
+  })
   
-  men_qualifying_scores[[country]] <- country_scores
+  qualifying_df <- as.data.frame(do.call(rbind, qualifying_scores))
+  colnames(qualifying_df) <- apparatus_list
+  rownames(qualifying_df) <- names(data_list)
+  
+  return(qualifying_df)
 }
 
-men_qualifying_df <- as.data.frame(do.call(rbind, men_qualifying_scores))
-colnames(men_qualifying_df) <- apparatus_list
-selected_athletes_df <- data.frame(Country = names(men_country_athlete_list_12), FirstName = selected_athletes)
+qual_men_apparatuses <- c('HB', 'PH', 'FX', 'PB', 'SR', 'VT')
+qual_women_apparatuses <- c('BB', 'FX', 'UB', 'VT')
+men_qualifying_df <- process_data(men_country_athlete_list_12, qual_men_apparatuses)
+women_qualifying_df <- process_data(women_country_athlete_list_12, qual_women_apparatuses)
 
-# Compute the selected athletes' average score for each apparatus
-selected_athletes_scores <- list()
-
-for (athlete in selected_athletes_df$FirstName) {
-  athlete_data <- filter(men_athletes_data, FirstName == athlete)
-  athlete_scores <- c()
-  
-  for (apparatus in apparatuses) {
-    apparatus_data <- filter(athlete_data, Apparatus == apparatus)
-    if (nrow(apparatus_data) == 0) {
-      athlete_scores <- c(athlete_scores, 10)
-    } else {
-      athlete_scores <- c(athlete_scores, mean(apparatus_data$Score, na.rm = TRUE))
-    }
-  }
-  
-  selected_athletes_scores[[athlete]] <- athlete_scores
-}
-
-selected_athletes_scores_df <- as.data.frame(do.call(rbind, selected_athletes_scores))
-colnames(selected_athletes_scores_df) <- apparatuses
-rownames(selected_athletes_scores_df) <- selected_athletes_df$FirstName
-men_selected_athletes_df <- selected_athletes_scores_df
-
-apparatus_list <- c('BB', 'FX', 'UE', 'VT')
-women_qualifying_scores <- list()
-selected_athletes <- c()
-
-# Compute the selected athletes' average score for each apparatus
-for (country in names(women_country_athlete_list_12)) {
-  country_scores <- c()
-  country_athlete_data <- women_country_athlete_list_12[[country]]
-  selected_athlete <- country_athlete_data %>%
-    group_by(FirstName) %>%
-    summarize(total_score = sum(Score)) %>%
-    arrange(-total_score) %>%
-    slice(1) %>%
-    pull(FirstName)
-  
-  selected_athletes <- c(selected_athletes, selected_athlete)
-  
-  for (apparatus in apparatus_list) {
-    apparatus_data <- filter(country_athlete_data, Apparatus == apparatus)
-    if (nrow(apparatus_data) == 0) {
-      country_scores <- c(country_scores, 10)
-    } else {
-      top_2_athletes <- apparatus_data %>%
-        group_by(FirstName) %>%
-        summarize(average_score = mean(Score)) %>%
-        arrange(-average_score) %>%
-        slice(1:2) %>%
-        pull(average_score)
-      
-      selected_score <- filter(country_athlete_data, FirstName == selected_athlete, Apparatus == apparatus) %>%
-        pull(Score)
-      
-      top_3_scores <- c(top_2_athletes, selected_score)
-      country_scores <- c(country_scores, mean(top_3_scores, na.rm = TRUE))
-    }
-  }
-  
-  women_qualifying_scores[[country]] <- country_scores
-}
-
-women_qualifying_df <- as.data.frame(do.call(rbind, women_qualifying_scores))
-colnames(women_qualifying_df) <- apparatus_list
-selected_athletes_df <- data.frame(Country = names(women_country_athlete_list_12), FirstName = selected_athletes)
-
-# Compute the selected athletes' average score for each apparatus
-selected_athletes_scores <- list()
-
-for (athlete in selected_athletes_df$FirstName) {
-  athlete_data <- filter(women_athletes_data, FirstName == athlete)
-  athlete_scores <- c()
-  
-  for (apparatus in apparatus_list) {
-    apparatus_data <- filter(athlete_data, Apparatus == apparatus)
-    if (nrow(apparatus_data) == 0) {
-      athlete_scores <- c(athlete_scores, 10)
-    } else {
-      athlete_scores <- c(athlete_scores, mean(apparatus_data$Score, na.rm = TRUE))
-    }
-  }
-  
-  selected_athletes_scores[[athlete]] <- athlete_scores
-}
-
-selected_athletes_scores_df <- as.data.frame(do.call(rbind, selected_athletes_scores))
-colnames(selected_athletes_scores_df) <- apparatus_list
-rownames(selected_athletes_scores_df) <- selected_athletes_df$FirstName
-women_selected_athletes_df <- selected_athletes_scores_df
-
-# Select the top 8 countries with the highest total score on all apparatuses for men and women, separately
-men_total_scores <- rowSums(men_qualifying_df, na.rm = TRUE)
-women_total_scores <- rowSums(women_qualifying_df, na.rm = TRUE)
+# Team final
+men_total_scores <- rowSums(men_qualifying_df)
+women_total_scores <- rowSums(women_qualifying_df)
 
 top_8_men_countries <- names(sort(men_total_scores, decreasing = TRUE)[1:8])
 top_8_women_countries <- names(sort(women_total_scores, decreasing = TRUE)[1:8])
 
-# Create dataframes for the top 8 men's and women's countries with columns "Country", "Score", "Rank"
-men_team_final_df <- data.frame(Country = top_8_men_countries, 
-                                Score = men_total_scores[top_8_men_countries], 
-                                Rank = 1:8)
-women_team_final_df <- data.frame(Country = top_8_women_countries, 
-                                  Score = women_total_scores[top_8_women_countries], 
-                                  Rank = 1:8)
+men_team_final_df <- data.frame(Country = top_8_men_countries, Score = men_total_scores[top_8_men_countries], Rank = 1:8)
+women_team_final_df <- data.frame(Country = top_8_women_countries, Score = women_total_scores[top_8_women_countries], Rank = 1:8)
 
-print("men's team final outcome:")
+print("Men's team final outcome:")
 print(men_team_final_df)
-print("women's team final outcome:")
+print("Women's team final outcome:")
 print(women_team_final_df)
 
-
-# Append a total score column
-women_selected_athletes_df$TotalScore <- rowSums(women_selected_athletes_df[, c('BB', 'FX', 'UE', 'VT')], na.rm = TRUE)
-men_selected_athletes_df$TotalScore <- rowSums(men_selected_athletes_df[, c('BB', 'FX', 'UE', 'VT', 'VT1', 'VT2')], na.rm = TRUE)
-
-# Determine the men's ranking for each apparatus and append a "Rank" column for each apparatus
-apparatus_list_men <- c('BB', 'FX', 'UE', 'VT', 'VT1', 'VT2')
-for (apparatus in apparatus_list_men) {
-  men_selected_athletes_df[paste(apparatus, "Rank", sep = "")] <- as.integer(rank(-men_selected_athletes_df[[apparatus]], ties.method = "min"))
+# Individual final
+calculate_individual_scores <- function(data_list, apparatus_list) {
+  scores <- lapply(names(data_list), function(country) {
+    country_data <- data_list[[country]]
+    selected_athlete <- country_data %>%
+      group_by(FirstName) %>%
+      summarize(total_score = sum(Score)) %>%
+      arrange(-total_score) %>%
+      slice(1) %>%
+      pull(FirstName)
+    
+    sapply(apparatus_list, function(apparatus) {
+      apparatus_data <- subset(country_data, FirstName == selected_athlete & Apparatus == apparatus)
+      if (nrow(apparatus_data) == 0) return(10)
+      mean(apparatus_data$Score)
+    })
+  })
+  
+  scores_df <- as.data.frame(do.call(rbind, scores))
+  colnames(scores_df) <- apparatus_list
+  rownames(scores_df) <- names(data_list)
+  
+  scores_df$TotalScore <- rowSums(scores_df)
+  return(scores_df)
 }
 
-# Append a "TotalRank" column
-men_selected_athletes_df$TotalRank <- as.integer(rank(-men_selected_athletes_df$TotalScore, ties.method = "min"))
+men_individual_scores_df <- calculate_individual_scores(men_country_athlete_list_12, qual_men_apparatuses)
+women_individual_scores_df <- calculate_individual_scores(women_country_athlete_list_12, qual_women_apparatuses)
 
-print("men's individual final:")
-print(men_selected_athletes_df)
+men_individual_scores_df$TotalRank <- rank(-men_individual_scores_df$TotalScore, ties.method = "min")
+women_individual_scores_df$TotalRank <- rank(-women_individual_scores_df$TotalScore, ties.method = "min")
 
-# Determine the women's ranking for each apparatus and append a "Rank" column for each apparatus
-apparatus_list_women <- c('BB', 'FX', 'UE', 'VT')
-for (apparatus in apparatus_list_women) {
-  women_selected_athletes_df[paste(apparatus, "Rank", sep = "")] <- as.integer(rank(-women_selected_athletes_df[[apparatus]], ties.method = "min"))
-}
-
-# Append a "TotalRank" column
-women_selected_athletes_df$TotalRank <- as.integer(rank(-women_selected_athletes_df$TotalScore, ties.method = "min"))
-
-print("women's individual final:")
-print(women_selected_athletes_df)
-
+print("Men's individual final:")
+print(men_individual_scores_df)
+print("Women's individual final:")
+print(women_individual_scores_df)
