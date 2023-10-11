69ccbebe1ee948a8651fd55b486423641c762ac3
katareah <53327723+katareah@users.noreply.github.com>
2023-10-11 15:05:37 -0400

    very basic shiny app example on olympic dataset

--- app.R
+++ app.R
@@ -6,15 +6,16 @@
 #
 #    http://shiny.rstudio.com/
 #
-d = readRDS('tracts.census.2019.rds')
 
+library(tidyverse)
 library(shiny)
-
+d = read_csv('/Users/ruixiaowang/Desktop/Yale/2023 Fall/Case Studies/Week3Day2/women_total_number_of_medals_results.csv')
+view(d)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("2024 Olympic Gymnastic Competition Results"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -26,59 +27,42 @@ ui <- fluidPage(
             #             max = 50,
             #             value = 30),
             
-            selectInput(inputId='state', 
-                        label = 'State', 
-                        choices = sort(unique(d$state)), 
-                        selected = 'CT', 
-                        multiple = F)
+    
+            selectInput(inputId='Country Names', 
+                        label = 'Name', 
+                        choices = sort(unique(d$'Country Names')), 
+                        selected = c('GBR'), 
+                        multiple = T),
+            
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-          br(),
-           #plotOutput("distPlot"), 
-           #dataTableOutput('data'), 
-           
-          fluidRow(column(6, plotOutput('map')), 
-                   column(6, dataTableOutput('data')))
+          tableOutput('simTable'),  # Table to display Simulation1 and Simulation2
+          #plotOutput("distPlot"), 
+          #dataTableOutput('data'), 
+          fluidRow(
+            column(6, plotOutput('map')), 
+            column(6, dataTableOutput('data'))
+          )
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$simTable <- renderTable({
+    selected_countries <- d %>% filter(`Country Names` %in% input$'Country Names')
+    data.frame(
+      Country = selected_countries$`Country Names`,
+      Simulation1 = selected_countries$simulation1,  # Assume you have a column named Simulation1
+      Simulation2 = selected_countries$simulation2   # Assume you have a column named Simulation2
+    )
+  })
     
-    output$data <- renderDataTable({
-        faithful
-    })
     
-    output$map <- renderPlot({
-      
-      #d = d[d$state=='CT',]
-      d = d[d$state==input$state,]
-      dg = fortify(d, region='GEOID')
-      df = d@data %>%
-        select(GEOID, hh.income, house.value, age, pop)
-      dg = dg %>% 
-        left_join(df, by= c('id'='GEOID'))
-      head(dg)
-      
-      ggplot(dg, aes(x=long, y=lat, group=id, fill=hh.income))+
-        geom_polygon(color='black', show.legend = F, linewidth=0.25) + 
-        theme_pub(type='map')
-      
-    })
+  
     
 }
 
